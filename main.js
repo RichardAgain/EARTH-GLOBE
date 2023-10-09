7081d54fe6f4e80781221ab5b785498f721c101f
MoisesLondo <moiseslondo16@gmail.com>
2023-10-08 21:02:06 -0400

    Responsive

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera ( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+if (window.innerWidth < 768) {
+    camera.position.setZ(20);
+  } else {
+    camera.position.setZ(30);
+  }
+
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg')
 });
