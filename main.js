3dc84764eb053bef8eb6c939250edf7e7977a5bb
Ricardo Hernandez <ric.and.mail@gmail.com>
2023-10-10 09:16:33 -0400

    normal map

--- main.js
+++ main.js
@@ -5,51 +5,45 @@ const camera = new THREE.PerspectiveCamera ( 75, window.innerWidth / window.inne
 
 if (window.innerWidth < 768) {
     camera.position.setZ(20);
-  } else {
+} else {
     camera.position.setZ(30);
-  }
+}
+
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+})
 
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg')
 });
 
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight);
-camera.position.setZ(30)
+camera.position.setZ(10)
 
-// const geometry = new THREE.SphereGeometry( 15, 32, 16 ); 
-// const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } )
-// const sphere = new THREE.Mesh( geometry, material ); scene.add( sphere );
 
-// scene.add(sphere);
-
-const bulb = new THREE.PointLight(0xffffff, 300, 0)
-bulb.position.set(0,0,25)
+const bulb = new THREE.PointLight(0xffffff, 100, 0)
+bulb.position.set(0,0,10)
 scene.add(bulb)
 
 // const ambient = new THREE.AmbientLight(0xffffff)
 // scene.add(ambient)
 
-// const helper = new THREE.PointLightHelper(bulb)
-// scene.add(helper)
-
-// const grid = new THREE.GridHelper(200,50)
-// scene.add(grid)
-
 const globeTexture = new THREE.TextureLoader().load('images/globe.jpg')
+const normalTexture = new THREE.TextureLoader().load('images/normal.jpg')
 
 const globe = new THREE.Mesh(
-    new THREE.SphereGeometry( 15, 32, 16 ),
-    new THREE.MeshStandardMaterial({ map: globeTexture})  
+    new THREE.SphereGeometry( 5, 16, 16 ),
+    new THREE.MeshStandardMaterial({ map: globeTexture, normalMap: normalTexture })  
 );
 
 scene.add(globe)
 
 renderer.render(scene, camera); 
-
-// const controls = new OrbitControls(camera, renderer.domElement);
 
 const spaceTexture = new THREE.TextureLoader().load('images/space.jpg');
 scene.background = spaceTexture; 
@@ -58,19 +52,7 @@ scene.background = spaceTexture;
 function animate() {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
-    // globe.rotation.x += 0.01;
-    globe.rotation.y += -0.01;
-    // globe.rotation.z += 0.01;
-
-
-    // controls.update()
+    globe.rotation.y += -0.005;
 }
 
 animate()
-
-window.addEventListener('resize', () => {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-
-    camera.updateProjectionMatrix();
-})
