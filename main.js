424465d5a35cadfb83637fec3f813d7736827dc4
MoisesLondo <moiseslondo16@gmail.com>
2023-10-11 01:45:45 -0400

    Update: Puntos, rotación

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-
+let boton = document.getElementById("pais1")
+boton.addEventListener("click", animate)
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera ( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -33,13 +34,19 @@ scene.add(bulb)
 // const ambient = new THREE.AmbientLight(0xffffff)
 // scene.add(ambient)
 
-const globeTexture = new THREE.TextureLoader().load('images/globe.jpg')
+const globeTexture = new THREE.TextureLoader().load('images/globePrueba.jpg')
 const normalTexture = new THREE.TextureLoader().load('images/normal.jpg')
 
 const globe = new THREE.Mesh(
     new THREE.SphereGeometry( 5, 16, 16 ),
     new THREE.MeshStandardMaterial({ map: globeTexture, normalMap: normalTexture })  
 );
+globe.rotation.y += 5.9;
+//camera.position.z += -2;
+
+const x = globe.position.x;
+globe.position.x = x;
+
 
 scene.add(globe)
 
@@ -49,15 +56,34 @@ const spaceTexture = new THREE.TextureLoader().load('images/space.jpg');
 scene.background = spaceTexture; 
 
 let z = 0;
-const zFinal = 1;
+const zFinal = 0.6;
+let zoom = 0;
+const zoomFinal = 1;
+const zoomVelocity = 0.005;
 
+function updateZoom() {
+    if (zoom < zoomFinal) {
+        zoom += zoomVelocity;
+    }
+    camera.zoom = zoom;
+    camera.updateProjectionMatrix();}
+
+
+    function argentina(){
+        z -= 0.01; // Cambio aquí
+        if (z > -zFinal) { // Cambio aquí
+            globe.rotation.x = z
+        }
+    
+        
+    }
 function animate() {
     requestAnimationFrame(animate)
-    z += 0.01;
-    if (z < zFinal) {
-        globe.rotation.x = z
-    }
-    renderer.render(scene, camera); 
+    updateZoom();
+    argentina();
+    renderer.render(scene, camera)
 }
 
 animate()
+
+//Podrías hacer que cuando el programa inicie haga un zoom in?
